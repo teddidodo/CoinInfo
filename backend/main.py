51a40356109e4d18130dad5da60f3780e791d0aa
Thanh Do <ducthanhdo.developer@gmail.com>
2023-11-18 14:12:36 +0100

    build first 2 apis

---
+++ backend/main.py
@@ -0,0 +1,47 @@
+import requests
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI, Response, status, HTTPException, Depends
+from sqlalchemy.orm import Session
+from database import get_db
+import model
+
+load_dotenv()
+
+app = FastAPI()
+
+@app.get("/exchange_rate/eth")
+async def get_btc_exchange_rate(db: Session = Depends(get_db)):
+    url = os.environ.get('COIN_API') + "exchange_rates"
+    params = {'id': 'eth'}
+    response = requests.get(url, params=params)
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail="Failed to fetch data")
+    
+    data = response.json()
+    eth_exchange_rate = {}
+    for item in data['rates']:
+        if item == 'eth':
+            eth_exchange_rate = data['rates'][item] 
+
+    new_rate = {'coin': 'ETH', 'rate': eth_exchange_rate['value']}
+    new_rate = model.ExchangeRate(**new_rate)
+    db.add(new_rate)
+    db.commit()
+    db.refresh(new_rate)
+
+    return {'rate': eth_exchange_rate}
+
+@app.get("/eth-price")
+async def get_eth_price():
+    url = os.environ.get('COIN_API') + 'coins/markets?vs_currency=usd&ids=ethereum'
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return {"eth_price": response.json()}
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Failed to fetch data")
+    
